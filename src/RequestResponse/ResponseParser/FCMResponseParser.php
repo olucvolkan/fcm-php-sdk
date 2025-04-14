685f58ba35c75cbbb55efa2f24bfa6baeefd3d95
Volkan <volkanoluc@gmail.com>
2025-04-14 16:55:33 +0200

    Add FCMResponseParser implementation

---
+++ src/RequestResponse/ResponseParser/FCMResponseParser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Firebase\CloudMessaging\RequestResponse\ResponseParser;
+
+use Firebase\CloudMessaging\RequestResponse\Exception\InvalidResponseException;
+
+class FCMResponseParser implements ResponseParserInterface
+{
+    /**
+     * Parse FCM response
+     * 
+     * @param string $response
+     * @return array|null
+     */
+    public function parse(string $response): ?array
+    {
+        if (empty($response)) {
+            return null;
+        }
+        
+        $parsedResponse = json_decode($response, true);
+        
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new InvalidResponseException('Failed to parse FCM response: ' . json_last_error_msg());
+        }
+        
+        return $parsedResponse;
+    }
+}
