1e5b409d152ddbfb09d54dc8f7764cfd35a61aed
Volkan <volkanoluc@gmail.com>
2025-04-14 17:03:14 +0200

    Update ApiResponseInterface for PHP 7.1 compatibility

--- src/RequestResponse/ApiResponseInterface.php
+++ src/RequestResponse/ApiResponseInterface.php
@@ -9,33 +9,33 @@ interface ApiResponseInterface
      * 
      * @return int
      */
-    public function getStatusCode(): int;
+    public function getStatusCode();
     
     /**
      * Get any error message from the request
      * 
      * @return string
      */
-    public function getErrorMessage(): string;
+    public function getErrorMessage();
     
     /**
      * Check if the request was successful
      * 
      * @return bool
      */
-    public function isSuccess(): bool;
+    public function isSuccess();
     
     /**
      * Get the raw response string
      * 
      * @return string
      */
-    public function getRawResponse(): string;
+    public function getRawResponse();
     
     /**
      * Get the parsed response
      * 
      * @return array|null
      */
-    public function getResult(): ?array;
+    public function getResult();
 }
