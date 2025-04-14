15df50b90f75acab2cac7c6ae6f7180c6298e20a
Volkan <volkanoluc@gmail.com>
2025-04-14 16:55:27 +0200

    Add ResponseParserInterface

---
+++ src/RequestResponse/ResponseParser/ResponseParserInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Firebase\CloudMessaging\RequestResponse\ResponseParser;
+
+interface ResponseParserInterface
+{
+    /**
+     * Parse a raw response string into a structured format
+     * 
+     * @param string $response
+     * @return array|null
+     */
+    public function parse(string $response): ?array;
+}
