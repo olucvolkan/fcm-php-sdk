9282745da127485238a286a8003e3129697aa3d6
Volkan <volkanoluc@gmail.com>
2025-04-14 16:55:43 +0200

    Add base FCMException class

---
+++ src/RequestResponse/Exception/FCMException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Firebase\CloudMessaging\RequestResponse\Exception;
+
+/**
+ * Base exception class for all FCM related exceptions
+ */
+class FCMException extends \Exception
+{
+}
