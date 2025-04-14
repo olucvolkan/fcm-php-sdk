50afc64ac8778376eff14e596c5390d20d5700f0
Volkan <volkanoluc@gmail.com>
2025-04-14 16:56:01 +0200

    Add InvalidMessageException class

---
+++ src/RequestResponse/Exception/InvalidMessageException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Firebase\CloudMessaging\RequestResponse\Exception;
+
+/**
+ * Exception thrown when an invalid message configuration is detected
+ */
+class InvalidMessageException extends FCMException
+{
+}
